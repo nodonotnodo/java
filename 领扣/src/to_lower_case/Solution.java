ccd45c6228247e6b741c1cb3ee6514279557eb65
nodonotnodo <18292026871@163.com>
2018-11-26 22:13:19 +0800

    字符串大写转小写

---
+++ 领扣/src/to_lower_case/Solution.java
@@ -0,0 +1,39 @@
+package to_lower_case;
+
+/*
+实现函数 ToLowerCase()，该函数接收一个字符串参数 str，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
+
+示例 1：
+输入: "Hello"
+输出: "hello"
+示例 2：
+输入: "here"
+输出: "here"
+示例 3：
+输入: "LOVELY"
+输出: "lovely"
+ */
+
+public class Solution {
+
+    public static String toLowerCase(String str) {
+        char[] newStr = new char[str.length()];
+        for(int i = 0; i<str.length(); i++){
+            if((str.charAt(i)>=65)&&(str.charAt(i)<=90)){
+                newStr[i] = (char)(str.charAt(i)+32);
+            }else{
+                newStr[i] = str.charAt(i);
+            }
+        }
+        String result = new String(newStr);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("hello"));
+        System.out.println(toLowerCase("Hello"));
+        System.out.println(toLowerCase("1234HI34"));
+        System.out.println(toLowerCase("HELLO"));
+    }
+
+}
