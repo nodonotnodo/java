164b0dc31264aa32cca0ffa648c5e6ac5afeca83
nodonotnodo <18292026871@163.com>
2018-12-12 22:12:08 +0800

    日志排序

---
+++ 领扣/src/reorder_log_files/Solution.java
@@ -0,0 +1,73 @@
+package reorder_log_files;
+
+/*
+* 你有一个日志数组 logs。每条日志都是以空格分隔的字串。
+对于每条日志，其第一个字为字母数字标识符。然后，要么：
+标识符后面的每个字将仅由小写字母组成，或；
+标识符后面的每个字将仅由数字组成。
+我们将这两种日志分别称为字母日志和数字日志。保证每个日志在其标识符后面至少有一个字。
+将日志重新排序，使得所有字母日志都排在数字日志之前。字母日志按字母顺序排序，忽略标识符，标识符仅用于表示关系。数字日志应该按原来的顺序排列。
+返回日志的最终顺序。
+
+示例 ：
+输入：["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+输出：["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+
+提示：
+0 <= logs.length <= 100
+3 <= logs[i].length <= 100
+logs[i] 保证有一个标识符，并且标识符后面有一个字
+* */
+
+public class Solution {
+
+    public static String[] reorderLogFiles(String[] logs) {
+        if(null == logs){
+            return null;
+        }
+        int num1 = 0;
+        int num2 = 0;
+        String[] logs1 = new String[logs.length];
+        String[] logs2 = new String[logs.length];
+        for(int i = 0; i<logs.length; i++){
+            if(num1+num2 == logs.length){
+                break;
+            }
+            char c = logs[i].charAt(logs[i].length()-1);
+            //如果是英文日志，那么将它放在英文字符的地方。
+            if(Character.isLetter(c)){
+                logs1[num1] = logs[i];
+                num1++;
+            }else{
+                logs2[num2] = logs[i];
+                num2++;
+            }
+        }
+        for(int i = 0; i<num1-1; i++){
+            int flag = 1;
+            for(int j = 0; j<num1-1-i; j++){
+                if(logs1[j].split(" ",2)[1].compareTo(logs1[j+1].split(" ",2)[1])>0){
+                    String tmp = logs1[j];
+                    logs1[j] = logs1[j+1];
+                    logs1[j+1] = tmp;
+                    flag = 0;
+                }
+            }
+            if(flag == 1){
+                break;
+            }
+        }
+        for(int i = 0; i<num1; i++){
+            logs[i] = logs1[i];
+        }
+        for(int i = num1;i<logs.length; i++){
+            logs[i] = logs2[i-num1];
+        }
+        return logs;
+    }
+
+    public static void main(String[] args) {
+        String[] logs = new String[]{"a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"};
+        reorderLogFiles(logs);
+    }
+}
